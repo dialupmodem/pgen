4298227a9866fb02ce6a07f75eee740890c36d77
dialupmodem <dialup.modem113@gmail.com>
2022-05-15 17:15:17 -0400

    add primevue, primeicons, primeflex, empty home component

--- src/main.js
+++ src/main.js
@@ -1,4 +1,15 @@
 import { createApp } from 'vue'
 import App from './App.vue'
 
-createApp(App).mount('#app')
+import PrimeVue from 'primevue/config'
+
+import 'primevue/resources/primevue.min.css'
+import 'primeicons/primeicons.css'
+import 'primeflex/primeflex.css'
+import 'primevue/resources/themes/saga-blue/theme.css'
+
+
+const app = createApp(App)
+
+app.use(PrimeVue)
+
